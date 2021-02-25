eeadfe6e6b6aab99d0911701929d1a4812b30b0e
borbelybalint <bbalint0404@gmail.com>
2021-02-25 09:52:28 +0100

    Kígyó fej létrehozása és előre mozgatása

--- snake.py
+++ snake.py
@@ -8,6 +8,12 @@ palya.title("Snake")
 palya.tracer(0)  # animáció sebessége
 palya.listen()
 
+fej = turtle.Turtle()
+fej.shape("triangle")
+fej.penup()
+fej.color("black")
+
 while True:
+    fej.forward(20)
     palya.update()
     time.sleep(0.3)
